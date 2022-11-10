a6c9ceb8780882b505deee8f9901007088671341
Javier Vazquez Fernandez <javier@MacBook-Pro-de-Javier-2.local>
2022-11-10 14:03:14 +0100

    [F] Calculate essence since last update

--- src/apps/CryptoWars/frontend/src/Pages/Town/TownHeader.tsx
+++ src/apps/CryptoWars/frontend/src/Pages/Town/TownHeader.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { useEffect, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -7,11 +6,42 @@ import Typography from '@mui/material/Typography';
 import { usePlayer } from './usePlayer';
 import { useParams } from 'react-router-dom';
 import { styled } from '@mui/material';
+import { TownPrimitives } from '../../../../../../Contexts/CryptoWars/Towns/domain/Town';
 
 const HeaderWrapper = styled(AppBar)(({ theme }) => ({
   marginBottom: theme.spacing(3),
   backgroundColor: theme.palette.grey.A700
 }));
+
+const calculateCurrentAssetValue = (
+  initial: number,
+  generationPerHour: number,
+  hoursToUpdate: number
+): number => parseFloat((initial + generationPerHour * hoursToUpdate).toFixed(0));
+
+const fromMsToHours = (ms: number): number => {
+  const sec = ms / 1000;
+  return sec / 3600;
+};
+
+const calculateMsSince = (date: Date) => Math.abs(new Date().valueOf() - date.valueOf());
+
+const calculateInitialEssenceOf = (town: TownPrimitives): number => {
+  const { generationPerHour } = town.buildings.essenceGenerator;
+  const essenceWarehouse = town?.buildings.warehouse.assets.find(
+    asset => asset.name === 'essence' // todo: we need the enum from the backend
+  );
+  const lastEssenceRegistry = essenceWarehouse?.stored ?? 0;
+  const lastEssenceUpdate = essenceWarehouse?.lastStorageUpdate ?? new Date();
+
+  return calculateCurrentAssetValue(
+    lastEssenceRegistry,
+    generationPerHour,
+    fromMsToHours(calculateMsSince(new Date(lastEssenceUpdate)))
+  );
+};
+
+const msToRecalculateEssence = 1000;
 
 export default function TownHeader() {
   const { result } = usePlayer();
@@ -19,20 +49,24 @@ export default function TownHeader() {
   const [essence, setEssence] = useState<number>(0);
 
   useEffect(() => {
-    const town = result?.player.towns.find(to => to.id === id);
-    const generation = town?.buildings.essenceGenerator.generationPerHour;
-    const e =
-      town?.buildings.warehouse.assets.find(asset => asset.name.toString() === 'essence')?.stored ??
-      0;
-    setEssence(e);
-    const refreshTimeInMs = 3000;
-    const refreshTimeInSec = refreshTimeInMs / 1000;
-    const refreshTimeInHours = refreshTimeInSec / 3600;
-    setInterval(() => {
-      if (generation) {
-        setEssence(ess => parseFloat((ess + generation * refreshTimeInHours).toFixed(2)));
+    if (result) {
+      const town = result.player.towns.find(to => to.id === id);
+
+      if (town) {
+        const { generationPerHour } = town.buildings.essenceGenerator;
+        setEssence(calculateInitialEssenceOf(town));
+        const updateEssence = () => {
+          setEssence(ess =>
+            calculateCurrentAssetValue(
+              ess,
+              generationPerHour,
+              fromMsToHours(msToRecalculateEssence)
+            )
+          );
+        };
+        setInterval(updateEssence, msToRecalculateEssence);
       }
-    }, refreshTimeInMs);
+    }
   }, [result]);
 
   return (
